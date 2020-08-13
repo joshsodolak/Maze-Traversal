edca76e29fc18bdb298d5701579868a369c9737a
Josh Sodolak <sodolakjoshua@gmail.com>
2020-08-13 10:58:54 -0500

    KeyEvent Works!

--- MazeTraversalPanel.java
+++ MazeTraversalPanel.java
@@ -5,8 +5,10 @@ import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class MazeTraversalPanel extends JPanel {
+public class MazeTraversalPanel extends JPanel implements KeyListener {
     private MazeTraversalFrame frame;
     private MazeTraversalTile[][] maze;
 
@@ -26,7 +28,7 @@ public class MazeTraversalPanel extends JPanel {
                 }
                 maze[i][j] = button.tile;
                 add(button);
-
+                button.addKeyListener(this);
                 button.addMouseListener(new MouseAdapter() {
                     public void mousePressed(MouseEvent e) {
                         switch (button.tile.getValue()) {
@@ -76,5 +78,27 @@ public class MazeTraversalPanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            frame.getModel().breadthFirstSearch();
+            System.out.println("Enter pressed! Begin BFS");
+        } else {
+            System.out.println("Key other than enter pressed!");
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
     }
 }
